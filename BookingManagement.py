88458b02484c2b9e94c3e4a3f9f9543fd56c4d27
Will Hammill <20711987@students.latrobe.edu.au>
2025-08-19 21:13:37 +1000

    edited code and HTML link locations

--- BookingManagement.py
+++ BookingManagement.py
@@ -2,24 +2,26 @@ from flask import Flask, render_template
 import os
 from Classes import database
 
-
 from bookingConfirm import appointments
 
 basedir = os.path.abspath(os.path.dirname(__file__))
+
+
 def start_application():
-    application=Flask(__name__)
-    application.secret_key = 'password'  # Be cautious with hardcoded secrets in production
-    application.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{os.path.join(basedir, "database", "booking.sqlite3")}'
-    application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    booking_application = Flask(__name__)
+    booking_application.secret_key = 'password'  # Be cautious with hardcoded secrets in production
+    booking_application.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{os.path.join(basedir, "database", "booking.sqlite3")}'
+    booking_application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-    database.init_app(application)
-    application.register_blueprint(appointments)
+    database.init_app(booking_application)
+    booking_application.register_blueprint(appointments)
 
-    with application.app_context():
+    with booking_application.app_context():
         database.create_all()
 
-    return application
+    return booking_application
 
-if __name__ =='__main__':
-    application= start_application()
-    application.run(debug=True, host='0.0.0.0', port='8080')
+
+if __name__ == '__main__':
+    application = start_application()
+    application.run(debug=True, host='0.0.0.0', port=8080)
--- bookingConfirm.py
+++ bookingConfirm.py
@@ -7,27 +7,30 @@ appointments = Blueprint('appointments', __name__)
 
 
 @appointments.route('/appointments/create/<int:id>', methods=['GET', 'POST'])
-def create_appointment(appointment_id):
+def create_appointment():
     pets = Pet.query_all()
-    # services = Service.query.all()
     pets.name = request.form.get('pet_name')
     time_string = request.form.get('appointment_time').strip()
     date_string = request.form.get('appointment_date')
     appointment_date = datetime.strptime(date_string, '%Y-%m-%d').date()
     appointment_time = datetime.strptime(time_string, '%I:%M %p').strftime('%H:%M')
-    service_id = request.form.get('Service')
     assigned_employee = 'N/A'
     customer_first_name = request.form.get('customer_first_name')
     customer_last_name = request.form.get('customer_last_name')
-    new_appointment = Appointment(pet_name=pets.name, customer_first_name=customer_first_name, client_last_name=customer_last_name, appointment_date=appointment_date,
+    new_appointment = Appointment(pet_name=pets.name, customer_first_name=customer_first_name,
+                                  client_last_name=customer_last_name,
+                                  appointment_date=appointment_date,
                                   appointment_time=appointment_time,
-                                  service_id=service_id, assigned_employee=assigned_employee, status='canceled')
-    # reminder_time = appointment_time
-    date_notice = timedelta(days=1)
-    reminder_date = appointment_date - date_notice  # placeholder for determining 24 hour notification
-    new_message = reminderMessage(client_name=new_appointment.customer_first_name, client_number=new_appointment.customer_number,
-                                  pet_name=new_appointment.pet_name, appointment_date=new_appointment.appointment_date,
-                                  appointment_time=new_appointment.appointment_time, reminder_date=reminder_date)
+                                  assigned_employee=assigned_employee,
+                                  status='unknown')
+    date_notice = timedelta(days=1)  # code to determine date for sending reminder/confirmation messages
+    reminder_date = appointment_date - date_notice
+    new_message = reminderMessage(client_name=new_appointment.customer_first_name,
+                                  client_number=new_appointment.customer_number,
+                                  pet_name=new_appointment.pet_name,
+                                  appointment_date=new_appointment.appointment_date,
+                                  appointment_time=new_appointment.appointment_time,
+                                  reminder_date=reminder_date)
     database.session.add(new_appointment)
     database.session.add(new_message)
     database.session.commit()
@@ -35,22 +38,22 @@ def create_appointment(appointment_id):
 
 @appointments.route('/appointments/reminder/<int:id>', methods=['GET', 'POST'])
 def confirm_appointment(appointment_id):
-    appointments = Appointment.query.get_or_404(appointment_id)
+    appointment = Appointment.query.get_or_404(appointment_id)
     messages = reminderMessage.query_all(appointment_id)
     contact_num = '(03) 5442 8880'
     current_timedate = datetime.now()
     current_time = current_timedate.time()
-    currentdate = current_timedate.date()
+    current_date = current_timedate.date()
     message = f'Hello {messages.client_name}, This is a reminder of your appointment at K9-Deli for {messages.pet_name} scheduled for {messages.appointment_date} at {messages.appointment_time}. Reply with Y to confirm your appointment or N to cancel. if you need to reschedule please ring {contact_num} '
-    if current_time == '09:00' & currentdate == messages.reminder_date:
-        send_message(message, appointments.client_number)
+    if current_time == '09:00' & current_date == messages.reminder_date:
+        send_message(message, appointment.client_number)
     message_response = receive_message()
     if message_response == 'Y':
-        appointments.status = 'confirmed'
+        appointment.status = 'confirmed'
         database.session.commit()
-    else:
-        database.session.remove(appointments)
-        # appointments.status = 'canceled'
+    elif message_response == 'N' or message_response is None:
+        # database.session.remove(appointments)
+        appointment.status = 'canceled'
         database.session.commit()
 
 
