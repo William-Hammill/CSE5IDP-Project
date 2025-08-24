02f51316214aa79943f754d61598cd59bf4c4700
Will Hammill <20711987@students.latrobe.edu.au>
2025-08-24 14:50:14 +1000

    modified some functions to use sqlite instead of SQLAcademy

--- BookingManagement.py
+++ BookingManagement.py
@@ -36,7 +36,6 @@ def start_application():
 
     return booking_application
 
-
 if __name__ == '__main__':
     application = start_application()
     application.run(debug=True, host='0.0.0.0', port=8080)
