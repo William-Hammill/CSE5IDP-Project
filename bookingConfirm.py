59435d794b4bf7a068074f2276871547afe8eed3
Will Hammill <20711987@students.latrobe.edu.au>
2025-08-27 13:46:44 +1000

    removed SQLacademy code

--- bookingConfirm.py
+++ bookingConfirm.py
@@ -1,12 +1,10 @@
 from flask import Blueprint, render_template, request, redirect, url_for
-# from Classes import Appointment, Pet, database, reminderMessage
 from datetime import datetime, timedelta
 from Messaging import send_message, receive_message, send_placeholder, recieve_placeholder
 import sqlite3
 
 appointments = Blueprint('appointments', __name__)
-#conn = sqlite3.connect('appointments-old.db')
-#c = conn.cursor()
+
 
 
 @appointments.route('/appointments')
@@ -28,7 +26,6 @@ def view_appointments():  # SQLITE version from simple_form branch
 @appointments.route('/create_appointment', methods=['POST'])
 def create_appointment():
     conn = sqlite3.connect('appointments.db')
-    # pets = Pet.query_all()
     pets_name = request.form['pet_name']
     appointment_time = request.form['appt_time'].strip()
     appt_date = request.form['appt_date']
@@ -39,24 +36,9 @@ def create_appointment():
     customer_last_name = request.form['customer_last']
     comments = request.form['comments']
     appointment_status = 1  # 1 = scheduled, 0 = canceled, 2 = confirmed
-    # new_appointment = Appointment(pet_name=pets.name, customer_first_name=customer_first_name,
-    #                               client_last_name=customer_last_name,
-    #                               appointment_date=appointment_date,
-    #                               appointment_time=appointment_time,
-    #                               assigned_employee=assigned_employee,
-    #                               status='unknown')
     date_notice = timedelta(days=1)  # code to determine date for sending reminder/confirmation messages
     reminder_date = appointment_date - date_notice # sets reminder send-date to day before appointment booking
     send_date = datetime.strftime(reminder_date,'%Y-%m-%d')
-    # new_message = reminderMessage(client_name=new_appointment.customer_first_name,
-    #                               client_number=new_appointment.customer_number,
-    #                               pet_name=new_appointment.pet_name,
-    #                               appointment_date=new_appointment.appointment_date,
-    #                               appointment_time=new_appointment.appointment_time,
-    #                               reminder_date=reminder_date)
-    # database.session.add(new_appointment)
-    # database.session.add(new_message)
-    # database.session.commit()
     c = conn.cursor()
     c.execute('''
                INSERT INTO appointments (customer_first, customer_last, appt_time, appt_date, pet_name, comments, appt_status)
@@ -83,8 +65,6 @@ def confirm_appointment(appointment_id):
     conn = sqlite3.connect('appointments.db')
     conn.row_factory = sqlite3.Row  # Enable dictionary-like row access in appointments_list.html
     c = conn.cursor()
-    # appointment = Appointment.query.get_or_404(appointment_id)
-    # messages = reminderMessage.query_all(appointment_id)
     contact_num = '(03) 5442 8880'
     current_timedate = datetime.now()
     current_time = current_timedate.time()
@@ -105,10 +85,9 @@ def confirm_appointment(appointment_id):
                 ''', (appointment_id,))
         conn.commit()
         conn.close()
-        # appointment.status = 'confirmed'
-        # database.session.commit()
+        thanks_message = 'Thank you for confirming your appointment with us'
+        send_placeholder(thanks_message, messages.customer_number)
     elif message_response == 'N' or message_response is None:
-        # database.session.remove(appointments)
         c.execute('''
                     UPDATE appointments 
                     SET appt_status = 0
@@ -116,22 +95,6 @@ def confirm_appointment(appointment_id):
                 ''', (appointment_id,))
         conn.commit()
         conn.close()
-        # appointment.status = 'canceled'
-    # database.session.commit()
-
-
-# @appointments.route('/appointments/view', methods=['POST'])
-# def view_appointments():  # SQLAcademy version
-# pets = Pet.query_all()
-# today = datetime.now().date()
-# upcoming_appointments = Appointment.query.filter(
-#    Appointment.date > today,
-#    Appointment.status != 'canceled'
-# ).join(Pet).all()
-
-# return render_template('AppointmentViewer.html',upcoming_appointments=upcoming_appointments,pets=pets)
-# return (url_for('AppointmentViewer'))
-
 
 # def allocate_employee(appointment_id, employee_id):
 # conn = sqlite3.connect('appointments-old.db')
@@ -146,9 +109,6 @@ def confirm_appointment(appointment_id):
 
 @appointments.route('/cancel/<int:appointment_id>')
 def cancel_appointment(appointment_id):
-    # selected_appointment = Appointment.query.get_or_404()
-    # selected_appointment.status = 'canceled'
-    # database.session.commit()
     conn = sqlite3.connect('appointments.db')
     c = conn.cursor()
     c.execute('''
