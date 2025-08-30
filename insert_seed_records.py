719e47d235590d957077396c03302233542ece21
sv-law <svasti.law0@gmail.com>
2025-08-30 22:59:29 +1000

    remove comment

--- insert_seed_records.py
+++ insert_seed_records.py
@@ -3,7 +3,6 @@ import sqlite3
 conn = sqlite3.connect('appointments.db')
 c = conn.cursor()
 
-# Try to add the phone_no column
 try:
     c.execute("INSERT INTO appointments (customer_first, customer_last, appt_time, appt_date, pet_name, phone_no, comments, appt_status) VALUES ('New', 'Guy', '09:00', '	2025-08-26', 'Titus', '0465100200', 'This is a sample comment', 1);")
     c.execute("INSERT INTO appointments (customer_first, customer_last, appt_time, appt_date, pet_name, phone_no, comments, appt_status) VALUES ('John', 'Doe', '14:00', '2025-08-27', 'King Joe', '0467100200', 'Example comment', 1);")
