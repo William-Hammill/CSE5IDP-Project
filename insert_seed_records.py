9d6970e8c2e39c0fcf479400953399e1769aeffe
sv-law <svasti.law0@gmail.com>
2025-08-30 22:54:07 +1000

    script for adding sample records

---
+++ insert_seed_records.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+conn = sqlite3.connect('appointments.db')
+c = conn.cursor()
+
+# Try to add the phone_no column
+try:
+    c.execute("INSERT INTO appointments (customer_first, customer_last, appt_time, appt_date, pet_name, phone_no, comments, appt_status) VALUES ('New', 'Guy', '09:00', '	2025-08-26', 'Titus', '0465100200', 'This is a sample comment', 1);")
+    c.execute("INSERT INTO appointments (customer_first, customer_last, appt_time, appt_date, pet_name, phone_no, comments, appt_status) VALUES ('John', 'Doe', '14:00', '2025-08-27', 'King Joe', '0467100200', 'Example comment', 1);")
+    print("Sample records added.")
+    # id_to_delete = 11
+    # c.execute('DELETE FROM appointments WHERE id = ?', (id_to_delete,))
+    # conn.commit()
+    # print('Delete successful')
+except sqlite3.OperationalError as e:
+    print("ERROR: Could not del", e)
+
+conn.commit()
+conn.close()
