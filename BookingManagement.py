8d1ba2d57ab148cc34fd74fd3b13efc9977bf9f6
Will Hammill <20711987@students.latrobe.edu.au>
2025-09-01 11:50:02 +1000

    modified confirm function

--- BookingManagement.py
+++ BookingManagement.py
@@ -7,13 +7,22 @@ booking_application = Flask(__name__)
 # basedir = os.path.abspath(os.path.dirname(__file__))
 @booking_application.route('/')
 def home():
-    current_datetime = datetime.now()
-    current_time = current_datetime.time()
-    if current_time == '9:00':
-        appointments.confirm_appointment()
-        return render_template('AppointmentViewer.html')
-    else:
-        return render_template('AppointmentViewer.html')
+    conn = sqlite3.connect('appointments.db')
+    conn.row_factory = sqlite3.Row  # Enable dictionary-like row access in appointments_list.html
+    c = conn.cursor()
+    c.execute('SELECT * FROM appointments')
+    booked_appointments = c.fetchall()
+    conn.close()
+    #current_datetime = datetime.now()
+   #current_time = current_datetime.time()
+    #current_date = current_datetime.date()
+    return render_template('AppointmentViewer.html', appointments=booked_appointments)
+    #if current_time == '9:00':
+    #    c.execute('SELECT * FROM reminders WHERE remindertime = ?'(current_date))
+    #    appointments.confirm_appointment()
+    #    return render_template('AppointmentViewer.html', appointments=booked_appointments)
+    #else:
+    #    return render_template('AppointmentViewer.html', appointments=booked_appointments)
 
 
 def init_db():
--- bookingConfirm.py
+++ bookingConfirm.py
@@ -38,11 +38,19 @@ def create_appointment():
     date_notice = timedelta(days=1)  # code to determine date for sending reminder/confirmation messages
     reminder_date = appointment_date - date_notice  # sets reminder send-date to day before appointment booking
     send_date = datetime.strftime(reminder_date, '%Y-%m-%d')
+    #appt_datetime = datetime.strptime(f"{appt_date} {appointment_time}", "%Y-%m-%d %H:%M")
+    # Check for existing appointments
+    # conn = sqlite3.connect('appointments.db')
     c = conn.cursor()
+    c.execute('SELECT * FROM appointments WHERE appt_date = ? AND appt_time = ?', (appt_date, appointment_time,))
+    existing_appointments = c.fetchall()
+    # conn.close()
+    if existing_appointments:
+        return "This appointment time is already booked. Please select another time.", 400
     c.execute('''
-               INSERT INTO appointments (customer_first, customer_last, appt_time, appt_date, pet_name, comments, appt_status)
-               VALUES (?, ?, ?, ?, ?, ?, ?)
-           ''', (
+                           INSERT INTO appointments (customer_first, customer_last, appt_time, appt_date, pet_name, comments, appt_status)
+                           VALUES (?, ?, ?, ?, ?, ?, ?)
+                       ''', (
         customer_first_name, customer_last_name, appointment_time, appt_date, pets_name, comments,
         appointment_status))
     print('Successfully added appointment')
@@ -50,16 +58,18 @@ def create_appointment():
     appointment_id = c.fetchone()
     appointment_reminder_id = [int(_) for _ in appointment_id]
     c.execute('''INSERT INTO reminders (customer_first, customer_last, customer_number, appt_time, 
-    appt_date, pet_name, reminder_date, appointment_id) VALUES (?,?,?,?,?,?,?,?)''',
+                appt_date, pet_name, reminder_date, appointment_id) VALUES (?,?,?,?,?,?,?,?)''',
               (customer_first_name, customer_last_name, customer_number, appointment_time, appt_date, pets_name,
                send_date, appointment_reminder_id[0]))
     conn.commit()
     print('Successfully added reminder')
     conn.close()
     return redirect(url_for('appointments.view_appointments'))
+    # c = conn.cursor()
 
 
-@appointments.route('/confirm_appointment/<int:id>', methods=['GET', 'POST'])
+
+@appointments.route('/confirm_appointment/<int:appointment_id>', methods=['GET', 'POST'])
 def confirm_appointment(appointment_id):
     conn = sqlite3.connect('appointments.db')
     conn.row_factory = sqlite3.Row  # Enable dictionary-like row access in appointments_list.html
@@ -68,33 +78,37 @@ def confirm_appointment(appointment_id):
     current_timedate = datetime.now()
     current_time = current_timedate.time()
     current_date = current_timedate.date()
-    if current_time == '09:00':  # & current_date == messages.reminder_date:
-        c.execute('''SELECT reminder_date, customer_first_name, customer_number, pets_name, appointment_date, 
-            appointment_time FROM reminders WHERE reminder_date = ? AND appointment_id = ?''', (current_date, appointment_id))
-        messages = c.fetchall()
-        message = f'Hello {messages[1]}, This is a reminder of your appointment at K9-Deli for {messages[3]} scheduled for {messages[4]} at {messages[5]}. Reply with Y to confirm your appointment or N to cancel. if you need to reschedule please ring {contact_num} '
-        send_placeholder(message, messages[2])
-        # send_message(message, appointment.customer_number)
-        message_response = recieve_placeholder()
-        if message_response == 'Y':
-            c.execute('''
-                    UPDATE appointments 
-                    SET appt_status = 2
-                    WHERE id = ?
-                ''', (appointment_id,))
-            conn.commit()
-            conn.close()
-            thanks_message = 'Thank you for confirming your appointment with us'
-            send_placeholder(thanks_message, messages.customer_number)
-        elif message_response == 'N' or message_response is None:
-            cancel_appointment(appointment_id)
-        # c.execute('''
-        #             UPDATE appointments
-        #             SET appt_status = 0
-        #             WHERE id = ?
-        #         ''', (appointment_id,))
-        # conn.commit()
-        # conn.close()
+    c.execute('''SELECT customer_first, customer_number, pet_name, appt_date, 
+                appt_time FROM reminders WHERE reminder_date = ? AND appointment_id = ?''',
+              (current_date, appointment_id))
+    messages = c.fetchall()
+    message = f'Hello {messages[0]}, This is a reminder of your appointment at K9-Deli for {messages[1]} scheduled for {messages[3]} at {messages[4]}. Reply with Y to confirm your appointment or N to cancel. if you need to reschedule please ring {contact_num} '
+    send_placeholder(message, messages[2])
+    # send_message(message, appointment.customer_number)
+    message_response = recieve_placeholder()
+    if message_response == 'Y':
+        c.execute('''
+                        UPDATE appointments 
+                        SET appt_status = 2
+                        WHERE id = ?
+                    ''', (appointment_id,))
+        conn.commit()
+        conn.close()
+        thanks_message = 'Thank you for confirming your appointment with us'
+        send_placeholder(thanks_message, messages.customer_number)
+    elif message_response == 'N' or message_response is None:
+        cancel_appointment(appointment_id)
+
+
+# if current_time == '09:00':  # & current_date == messages.reminder_date:
+
+# c.execute('''
+#             UPDATE appointments
+#             SET appt_status = 0
+#             WHERE id = ?
+#         ''', (appointment_id,))
+# conn.commit()
+# conn.close()
 
 
 # def allocate_employee(appointment_id, employee_id):
